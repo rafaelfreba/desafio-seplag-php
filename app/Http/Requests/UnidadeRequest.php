8dad4513294df5963b0bd226854c0cc6c5526ce6
Rafael Batista <rafaelfreba@gmail.com>
2025-03-28 05:16:17 -0400

    Adiciona validações e atributos para endereço na requisição de Unidade

--- app/Http/Requests/UnidadeRequest.php
+++ app/Http/Requests/UnidadeRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UnidadeRequest extends FormRequest
@@ -22,8 +23,13 @@ class UnidadeRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'unid_nome' => ['required','string','min:1','max:200'],
-            'unid_sigla' => ['required','string','min:1','max:20']
+            'unid_nome' => ['required', 'string', 'min:1', 'max:200'],
+            'unid_sigla' => ['required', 'string', 'min:1', 'max:20'],
+            'end_tipo_logradouro' => ['required', 'string', 'min:1', 'max:50'],
+            'end_logradouro' => ['required', 'string', 'min:1', 'max:200'],
+            'end_numero' => ['required', 'numeric'],
+            'end_bairro' => ['required', 'string', 'min:1', 'max:100'],
+            'cidade_id' => ['required', 'numeric', 'min:1', Rule::exists('cidades', 'id')]
         ];
     }
 
@@ -31,7 +37,12 @@ class UnidadeRequest extends FormRequest
     {
         return [
             'unid_nome' => 'Nome da Unidade',
-            'unid_sigla' => 'Sigla da Unidade'
+            'unid_sigla' => 'Sigla da Unidade',
+            'end_tipo_logradouro' => 'Tipo de Logradouro',
+            'end_logradouro' => 'Logradouro',
+            'end_numero' => 'Número',
+            'end_bairro' => 'Bairro',
+            'cidade_id' => 'Id da cidade',
         ];
     }
 }
