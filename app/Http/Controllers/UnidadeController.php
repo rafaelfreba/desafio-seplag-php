cc24547ea00636b1cdb1275cef9065a696980a9d
Rafael Batista <rafaelfreba@gmail.com>
2025-03-28 05:16:05 -0400

    Adiciona recursos de coleção e recurso para Unidade, atualiza métodos de controlador para retornar UnidadeCollection e UnidadeResource, e integra Endereco na criação e atualização de unidades

--- app/Http/Controllers/UnidadeController.php
+++ app/Http/Controllers/UnidadeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Unidade;
 use App\Http\Requests\UnidadeRequest;
+use App\Http\Resources\UnidadeCollection;
+use App\Http\Resources\UnidadeResource;
+use App\Models\Endereco;
 use Illuminate\Database\Eloquent\Collection;
 
 class UnidadeController extends Controller
@@ -13,24 +16,16 @@ class UnidadeController extends Controller
      *     path="/api/unidades",
      *     summary="Lista todas as unidades",
      *     description="Retorna todas as unidades cadastradas no sistema.",
+     *     tags={"Unidades"},
      *     @OA\Response(
      *         response=200,
      *         description="OK"
-     *     ),
-     *     @OA\Response(
-     *         response=400,
-     *         description="Erro na requisição",
      *     )
-     * )
-     * 
-     * Lista todas as unidades
-     *
-     * @param Unidade $unidades
-     * @return Collection
+     * )      
      */
-    public function index(Unidade $unidades): Collection
+    public function index(Unidade $unidades): UnidadeCollection
     {
-        return $unidades->all();
+        return new UnidadeCollection($unidades::with(['enderecos', 'enderecos.cidade'])->get());
     }
 
     /**
@@ -38,6 +33,7 @@ class UnidadeController extends Controller
      *     path="/api/unidades",
      *     summary="Cadastra uma nova unidade",
      *     description="Cria uma nova unidade com os dados fornecidos.",
+     *     tags={"Unidades"},
      *     @OA\RequestBody(
      *         required=true,
      *         @OA\MediaType(
@@ -46,6 +42,11 @@ class UnidadeController extends Controller
      *                 type="object",
      *                 @OA\Property(property="unid_nome", type="string", description="Nome da unidade", example="Unidade A"),
      *                 @OA\Property(property="unid_sigla", type="string", description="Sigla da unidade", example="AAA"),
+     *                 @OA\Property(property="end_tipo_logradouro", type="string", description="Tipo de logradouro", example="Avenida"),
+     *                 @OA\Property(property="end_logradouro", type="string", description="Logradouro", example="Alzira Santana"),
+     *                 @OA\Property(property="end_numero", type="integer", description="Número", example="100"),
+     *                 @OA\Property(property="end_bairro", type="integer", description="Bairro", example="Centro"),
+     *                 @OA\Property(property="cidade_id", type="integer", description="Id da Cidade", example="1"),
      *             )
      *         )
      *     ),
@@ -54,24 +55,26 @@ class UnidadeController extends Controller
      *         description="created",
      *         @OA\JsonContent(
      *             type="object",
-     *             @OA\Property(property="unid_nome", type="string", description="Nome da unidade", example="Unidade A"),
-     *             @OA\Property(property="unid_sigla", type="string", description="Sigla da unidade", example="AAA"),
+     *                 @OA\Property(property="unid_nome", type="string", description="Nome da unidade", example="Unidade A"),
+     *                 @OA\Property(property="unid_sigla", type="string", description="Sigla da unidade", example="AAA"),
+     *                 @OA\Property(property="end_tipo_logradouro", type="string", description="Tipo de logradouro", example="Avenida"),
+     *                 @OA\Property(property="end_logradouro", type="string", description="Logradouro", example="Alzira Santana"),
+     *                 @OA\Property(property="end_numero", type="integer", description="Número", example="100"),
+     *                 @OA\Property(property="end_bairro", type="integer", description="Bairro", example="Centro"),
+     *                 @OA\Property(property="cidade_id", type="integer", description="Id da Cidade", example="1"),
      *         )
-     *     ),
-     *     @OA\Response(
-     *         response=400,
-     *         description="Erro na requisição (dados inválidos)",
      *     )
      * )
-     * 
-     *  Cadastra uma nova unidade
-     *
-     * @param UnidadeRequest $request
-     * @return Unidade
      */
-    public function store(UnidadeRequest $request): Unidade
+    public function store(UnidadeRequest $request): UnidadeResource
     {
-        return Unidade::create($request->validated());
+        $unidade = Unidade::create($request->validated());
+
+        $endereco = Endereco::create($request->validated());
+
+        $unidade->enderecos()->attach($endereco->id);
+
+        return new UnidadeResource($unidade->load(['enderecos', 'enderecos.cidade']));
     }
 
     /**
@@ -79,6 +82,7 @@ class UnidadeController extends Controller
      *     path="/api/unidades/{id}",
      *     summary="Exibe uma unidade especificada",
      *     description="Retorna os detalhes de uma unidade com o ID fornecido.",
+     *     tags={"Unidades"},
      *     @OA\Parameter(
      *         name="id",
      *         in="path",
@@ -91,24 +95,20 @@ class UnidadeController extends Controller
      *         description="OK",
      *         @OA\JsonContent(
      *             type="object",
-     *             @OA\Property(property="unid_nome", type="string", description="Nome da unidade", example="Unidade A"),
-     *             @OA\Property(property="unid_sigla", type="string", description="Sigla da unidade", example="AAA"),
+     *                 @OA\Property(property="unid_nome", type="string", description="Nome da unidade", example="Unidade A"),
+     *                 @OA\Property(property="unid_sigla", type="string", description="Sigla da unidade", example="AAA"),
+     *                 @OA\Property(property="end_tipo_logradouro", type="string", description="Tipo de logradouro", example="Avenida"),
+     *                 @OA\Property(property="end_logradouro", type="string", description="Logradouro", example="Alzira Santana"),
+     *                 @OA\Property(property="end_numero", type="integer", description="Número", example="100"),
+     *                 @OA\Property(property="end_bairro", type="integer", description="Bairro", example="Centro"),
+     *                 @OA\Property(property="cidade_id", type="integer", description="Id da Cidade", example="1"),
      *         )
-     *     ),
-     *     @OA\Response(
-     *         response=404,
-     *         description="Unidade não encontrada",
      *     )
      * )
-     * 
-     * Exibe uma unidade especificada
-     *
-     * @param Unidade $unidade
-     * @return Unidade
      */
-    public function show(Unidade $unidade): Unidade
+    public function show(Unidade $unidade): UnidadeResource
     {
-        return $unidade;
+        return new UnidadeResource($unidade->load(['enderecos', 'enderecos.cidade']));
     }
 
     /**
@@ -116,6 +116,7 @@ class UnidadeController extends Controller
      *     path="/api/unidades/{id}",
      *     summary="Atualiza uma unidade especificada",
      *     description="Atualiza os dados de uma unidade existente com os dados fornecidos.",
+     *     tags={"Unidades"},
      *     @OA\Parameter(
      *         name="id",
      *         in="path",
@@ -129,8 +130,13 @@ class UnidadeController extends Controller
      *             mediaType="application/json",
      *             @OA\Schema(
      *                 type="object",
-     *                 @OA\Property(property="unid_nome", type="string", description="Nome da unidade", example="Unidade A"),
-     *             @OA\Property(property="unid_sigla", type="string", description="Sigla da unidade", example="AAA"),
+    *                 @OA\Property(property="unid_nome", type="string", description="Nome da unidade", example="Unidade A"),
+     *                 @OA\Property(property="unid_sigla", type="string", description="Sigla da unidade", example="AAA"),
+     *                 @OA\Property(property="end_tipo_logradouro", type="string", description="Tipo de logradouro", example="Avenida"),
+     *                 @OA\Property(property="end_logradouro", type="string", description="Logradouro", example="Alzira Santana"),
+     *                 @OA\Property(property="end_numero", type="integer", description="Número", example="100"),
+     *                 @OA\Property(property="end_bairro", type="integer", description="Bairro", example="Centro"),
+     *                 @OA\Property(property="cidade_id", type="integer", description="Id da Cidade", example="1"),
      *             )
      *         )
      *     ),
@@ -139,30 +145,38 @@ class UnidadeController extends Controller
      *         description="OK",
      *         @OA\JsonContent(
      *             type="object",
-     *             @OA\Property(property="unid_nome", type="string", description="Nome da unidade", example="Unidade A"),
-     *             @OA\Property(property="unid_sigla", type="string", description="Sigla da unidade", example="AAA"),
+     *                 @OA\Property(property="unid_nome", type="string", description="Nome da unidade", example="Unidade A"),
+     *                 @OA\Property(property="unid_sigla", type="string", description="Sigla da unidade", example="AAA"),
+     *                 @OA\Property(property="end_tipo_logradouro", type="string", description="Tipo de logradouro", example="Avenida"),
+     *                 @OA\Property(property="end_logradouro", type="string", description="Logradouro", example="Alzira Santana"),
+     *                 @OA\Property(property="end_numero", type="integer", description="Número", example="100"),
+     *                 @OA\Property(property="end_bairro", type="integer", description="Bairro", example="Centro"),
+     *                 @OA\Property(property="cidade_id", type="integer", description="Id da Cidade", example="1"),
      *         )
-     *     ),
-     *     @OA\Response(
-     *         response=400,
-     *         description="Erro na requisição (dados inválidos)",
      *     ),
      *     @OA\Response(
      *         response=404,
      *         description="No query results for model [App\\Models\\Unidade] [id]",
      *     )
      * )
-     * Atualiza uma unidade especificada
-     *
-     * @param UnidadeRequest $request
-     * @param Unidade $unidade
-     * @return Unidade
      */
-    public function update(UnidadeRequest $request, Unidade $unidade): Unidade
+    public function update(UnidadeRequest $request, Unidade $unidade): UnidadeResource
     {
         $unidade->update($request->validated());
 
-        return $unidade;
+        $endereco = Endereco::updateOrCreate(
+            [
+                'end_tipo_logradouro' => $request->safe()->end_tipo_logradouro,
+                'end_logradouro' => $request->safe()->end_logradouro,
+                'end_numero' => $request->safe()->end_numero,
+                'end_bairro' => $request->safe()->end_bairro,
+                'cidade_id' => $request->safe()->cidade_id,
+            ]
+        );
+
+        $unidade->enderecos()->sync([$endereco->id]);
+
+        return new UnidadeResource($unidade->load(['enderecos', 'enderecos.cidade']));
     }
 
     /**
@@ -170,6 +184,7 @@ class UnidadeController extends Controller
      *     path="/api/unidades/{id}",
      *     summary="Remove uma unidade especificada",
      *     description="Remove a unidade com o ID fornecido.",
+     *     tags={"Unidades"},
      *     @OA\Parameter(
      *         name="id",
      *         in="path",
@@ -179,22 +194,13 @@ class UnidadeController extends Controller
      *     ),
      *     @OA\Response(
      *         response=200,
-     *         description="OK",
-     *         @OA\JsonContent(
-     *             type="object",
-     *              @OA\Property(property="unid_nome", type="string", description="Nome da unidade", example="Unidade A"),
-     *          @OA\Property(property="unid_sigla", type="string", description="Sigla da unidade", example="AAA"),
-     *         )
+     *         description="OK"
      *     ),
      *     @OA\Response(
      *         response=404,
      *         description="No query results for model [App\\Models\\Unidade] [id]",
      *     )
      * )
-     * Remove uma unidade especificada
-     *
-     * @param Unidade $unidade
-     * @return array
      */
     public function destroy(Unidade $unidade): array
     {
