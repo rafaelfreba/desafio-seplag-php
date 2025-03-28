c73ccd05c2aec92998c225c35c2eb529de85401a
Rafael Batista <rafaelfreba@gmail.com>
2025-03-27 22:44:02 -0400

    Adiciona requisições de validação para EfetivoServidor e ServidorTemporario

---
+++ app/Http/Requests/EfetivoServidorRequest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class EfetivoServidorRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //servidor efetivo
+            'pessoa_id' => [request()->is('POST') ? 'nullable' : 'required','numeric','min:1'],
+            'se_matricula' => ['required','string','min:1','max:20'],
+            //pessoa
+            'pes_nome' => ['required','string','min:1','max:200'],
+            'pes_data_nascimento' => ['required','digits:4','after: 1908', 'before:'. date('Y')],
+            'pes_sexo' => ['required','string', 'max:9', Rule::in(['nasculino', 'feminino'])],
+            'pes_mae' => ['required','string','min:1','max:200'],
+            'pes_pai' => ['required','string','min:1','max:200'],
+            //endereco pessoa
+            'pes_end_tipo_logradouro' => ['required','string','min:1','max:50'],
+            'pes_end_logradouro' => ['required','string','min:1','max:200'],
+            'pes_end_numero' => ['required','numeric'],
+            'pes_end_bairro' => ['required','string','min:1','max:100'],
+            'pes_cid_nome' => ['required','string','min:1','max:100'],
+            'pes_cid_uf' => ['required','string','min:1','max:2'],
+            // //unidade
+            // 'unid_nome' => ['required','string','min:1','max:200'],
+            // 'unid_sigla' => ['required','string','min:1','max:20'],
+            // //endereco unidade
+            // 'un_end_tipo_logradouro' => ['required','string','min:1','max:50'],
+            // 'un_end_logradouro' => ['required','string','min:1','max:200'],
+            // 'un_end_numero' => ['required','numeric'],
+            // 'un_end_bairro' => ['required','string','min:1','max:100'],
+            // 'un_cid_nome' => ['required','string','min:1','max:100'],
+            // 'un_cid_uf' => ['required','string','min:1','max:2'],
+            //lotacao
+            'lot_data_lotacao' => ['required','date'],
+            'lot_data_remocao' => ['nullable','date'],
+            'lot_portaria' => ['required','string','min:1','max:20'],
+            //foto pessoa
+            'fp_data' => ['required','date'],
+            'fp_bucket' => ['required','string','min:1','max:100'],
+            'fp_hash' => ['required','string','min:1','max:100']
+        ];
+    }
+}
