d4a78e8c8dced498f000e77ab48f1e4d875f5a87
Rafael Batista <rafaelfreba@gmail.com>
2025-03-27 22:42:55 -0400

    Adiciona novas rotas de API e arquivos de tradução para autenticação e paginação em português

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\UnidadeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('teste', function () {
+    return ['message' => 'Teste de rota'];
+});
+
+Route::apiResource('unidades', UnidadeController::class);
