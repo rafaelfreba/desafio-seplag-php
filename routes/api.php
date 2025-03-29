4d0428c024f3da534318d874f7083c2c704a5c81
Rafael Batista <rafaelfreba@gmail.com>
2025-03-28 23:11:49 -0400

    Adiciona rotas de autenticação e gerenciamento de unidades com middleware de autorização

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,23 @@
 <?php
 
+use App\Http\Controllers\ApiAuthController;
 use App\Http\Controllers\UnidadeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('teste', function () {
-    return ['message' => 'Teste de rota'];
-});
+Route::post('/login', [ApiAuthController::class, 'login']);
 
-Route::apiResource('unidades', UnidadeController::class);
+Route::post('/refresh-token', [ApiAuthController::class, 'refreshToken'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('unidades', [UnidadeController::class, 'index'])->middleware('ability:view-unidades');
+    Route::get('unidades/{unidade}', [UnidadeController::class, 'show'])->middleware('ability:view-unidades');
+    Route::post('unidades', [UnidadeController::class, 'store'])->middleware('ability:create-unidades');
+    Route::put('unidades/{unidade}', [UnidadeController::class, 'update'])->middleware('ability:update-unidades');
+    Route::delete('unidades/{unidade}', [UnidadeController::class, 'destroy'])->middleware('ability:delete-unidades');
+});
