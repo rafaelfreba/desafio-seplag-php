18ebc53a888b4934f5c7f491637aa8f32f79623d
Rafael Batista <rafaelfreba@gmail.com>
2025-03-30 20:14:28 -0400

    Melhora a formatação das rotas de unidades para maior legibilidade

--- routes/unidades.php
+++ routes/unidades.php
@@ -6,9 +6,16 @@ use App\Http\Controllers\UnidadeController;
 // unidades
 Route::middleware('auth:sanctum')->group(function () {
 
-    Route::get('unidades', [UnidadeController::class, 'index'])->middleware('ability:view-unidades');
-    Route::get('unidades/{unidade}', [UnidadeController::class, 'show'])->middleware('ability:view-unidades');
-    Route::post('unidades', [UnidadeController::class, 'store'])->middleware('ability:create-unidades');
-    Route::put('unidades/{unidade}', [UnidadeController::class, 'update'])->middleware('ability:update-unidades');
+    Route::get('unidades', [UnidadeController::class, 'index'])
+    ->middleware('ability:view-unidades');
+
+    Route::get('unidades/{unidade}', [UnidadeController::class, 'show'])
+    ->middleware('ability:view-unidades');
+
+    Route::post('unidades', [UnidadeController::class, 'store'])
+    ->middleware('ability:create-unidades');
+
+    Route::put('unidades/{unidade}', [UnidadeController::class, 'update'])
+    ->middleware('ability:update-unidades');   
 
 });
